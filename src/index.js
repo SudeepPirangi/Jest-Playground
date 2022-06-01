33be3e632ed1b91c0b7320e17479c9cd3837aa13
SudeepPirangi <sudeep_edu@yahoo.com>
2022-06-01 23:21:50 +0530

    Adds single character - test case

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-const add = () => {};
+const add = (input) => {
+  if (!input.trim().length) return 0;
+  let inputArr = input.split(",");
+  console.log("inputArr", inputArr);
+  return getSum(inputArr);
+};
+
+function getSum(inputArr) {
+  return inputArr.reduce((total, num) => (isNaN(+num) ? "invalid input" : (total += +num)), 0);
+}
+
+// console.log('Addition of - ""', add("a"));
 
 module.exports = add;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,15 @@
 const add = require("../src/index");
 
 describe("Adds string numbers", () => {
-  test("Empty sting input", () => {
+  test("Empty string input - 0", () => {
     expect(add("")).toBe(0);
+  });
+
+  test("Single numeric character '1' - 1", () => {
+    expect(add("1")).toBe(1);
+  });
+
+  test("Non-numeric character 'a' - invalid input", () => {
+    expect(add("a")).toBe("invalid input");
   });
 });
