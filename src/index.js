02060ccedf27ddda989e6e3c6ac503e408098906
SudeepPirangi <sudeep_edu@yahoo.com>
2022-06-02 01:44:17 +0530

    Handles -ve numbers and throws exceptions

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ const add = (input) => {
 };
 
 /**
- * Replaces '\n' characters with ',' and converts string input to array of numbers
+ * Identifies delimiter if the string has anything included
+ * Replaces '\n' characters with delimiter
+ * Splits final string input to array of numbers
  * @param {string} input
  * @returns characters_as_array
  */
@@ -27,21 +29,28 @@ function stringToArray(input) {
 
 /**
  * Function to calculate sum of numbers in input array
+ * return 'invalid input' if array has any non-numeric
+ * throws error if array has any negative numbers
  * @param {string} inputArr
  * @returns sum_of_elements
  */
 function getSum(inputArr) {
   var sum = 0;
+  var negatives = [];
   for (let i = 0; i < inputArr.length; ++i) {
     if (isNaN(inputArr[i])) {
       sum = "invalid input";
       break;
     }
-    sum += inputArr[i];
+    if (inputArr[i] < 0) negatives.push(inputArr[i]);
+    else sum += inputArr[i];
+  }
+  if (negatives.length) {
+    throw new Error(`negatives not allowed - ${negatives.join(", ")}`);
   }
   return sum;
 }
 
-// console.log('Addition of - "//;100;20;a"', add("//;2\n20;9"));
+// console.log('Addition of - "//;2\n20;9"', add("//;2\n20;9"));
 
-module.exports = { add, stringToArray };
+module.exports = { add, stringToArray, getSum };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,32 +1,6 @@
-const { add, stringToArray } = require("../src/index");
+const { add, stringToArray, getSum } = require("../src/index");
 
-describe("String to array convertion", () => {
-  test("Alphabetic string 'a,b,c' - [NaN, NaN, NaN]", () => {
-    expect(stringToArray("a,b,c")).toEqual([NaN, NaN, NaN]);
-  });
-
-  test("Alphabetic string '90\n20,c' - [90, 20, NaN]", () => {
-    expect(stringToArray("90\n20,c")).toEqual([90, 20, NaN]);
-  });
-
-  test("Numeric string '90\n20,50' - [90, 20, 50]", () => {
-    expect(stringToArray("90\n20\n50")).toEqual([90, 20, 50]);
-  });
-
-  test("New delimiter with combination of '\n' '//;100\n20;50' - [100, 20, 50]", () => {
-    expect(stringToArray("//;100\n20;50")).toEqual([100, 20, 50]);
-  });
-
-  test("New delimiter with combination of '\n' and non-numeric '//&100\n20&a' - [100, 20, NaN]", () => {
-    expect(stringToArray("//&100\n20&a")).toEqual([100, 20, NaN]);
-  });
-
-  test("New delimiter '//*2*20\n9' - [2, 20, 9]", () => {
-    expect(stringToArray("//*2*20\n9")).toEqual([2, 20, 9]);
-  });
-});
-
-describe("Adds string numbers", () => {
+describe("Adds simple/generic numbers", () => {
   test("Empty string input - 0", () => {
     expect(add("")).toBe(0);
   });
@@ -65,5 +39,77 @@ describe("Adds string numbers", () => {
 
   test("New-line chars in input '1\n2,3' - 6", () => {
     expect(add("1\n2,3")).toBe(6);
+  });
+});
+
+describe("String to array convertion", () => {
+  test("Alphabetic string 'a,b,c' - [NaN, NaN, NaN]", () => {
+    expect(stringToArray("a,b,c")).toEqual([NaN, NaN, NaN]);
+  });
+
+  test("Alphabetic string '90\n20,c' - [90, 20, NaN]", () => {
+    expect(stringToArray("90\n20,c")).toEqual([90, 20, NaN]);
+  });
+
+  test("Numeric string '90\n20,50' - [90, 20, 50]", () => {
+    expect(stringToArray("90\n20\n50")).toEqual([90, 20, 50]);
+  });
+
+  test("New delimiter with combination of '\n' '//;100\n20;50' - [100, 20, 50]", () => {
+    expect(stringToArray("//;100\n20;50")).toEqual([100, 20, 50]);
+  });
+
+  test("New delimiter with combination of '\n' and non-numeric '//&100\n20&a' - [100, 20, NaN]", () => {
+    expect(stringToArray("//&100\n20&a")).toEqual([100, 20, NaN]);
+  });
+
+  test("New delimiter '//*2*20\n9' - [2, 20, 9]", () => {
+    expect(stringToArray("//*2*20\n9")).toEqual([2, 20, 9]);
+  });
+
+  test("New delimiter '//*2*-2\n9' - [2, -2, 9]", () => {
+    expect(stringToArray("//*2*-2\n9")).toEqual([2, -2, 9]);
+  });
+
+  test("Complex delimiter '//#-32#-12\n9#93\n-8' - [-32, -12, 9, 93, -8]", () => {
+    expect(stringToArray("//#-32#-12\n9#93\n-8")).toEqual([-32, -12, 9, 93, -8]);
+  });
+
+  test("Regular delimiter '-32\n-12,-8' - [-32, -12, -8]", () => {
+    expect(stringToArray("-32\n-12,-8")).toEqual([-32, -12, -8]);
+  });
+});
+
+describe("Addition of elements in array", () => {
+  test("Sum of [10] => 10", () => {
+    expect(getSum([10])).toBe(10);
+  });
+
+  test("Sum of [10, NaN] => invalid input", () => {
+    expect(getSum([10, NaN])).toBe("invalid input");
+  });
+
+  test("Sum of [10, 20] => 30", () => {
+    expect(getSum([10, 20])).toBe(30);
+  });
+
+  test("Sum of [90, 20, NaN] => invalid input", () => {
+    expect(getSum([90, 20, NaN])).toBe("invalid input");
+  });
+
+  test("Sum of [90, 20, 50] => 160", () => {
+    expect(getSum([90, 20, 50])).toBe(160);
+  });
+
+  test("Sum of [2, -2, 9] => negatives not allowed - -2", () => {
+    expect(getSum([2, -2, 9])).toThrow(/negatives not allowed/);
+  });
+
+  test("Sum of [-32, -12, 9, 93, -8] => negatives not allowed - -32, -12, -8", () => {
+    expect(getSum([-32, -12, 9, 93, -8])).toThrow("negatives not allowed - -32, -12, -8");
+  });
+
+  test("Sum of [-32, -12, -8] => negatives not allowed - -32, -12, -8", () => {
+    expect(getSum([-32, -12, -8])).toThrow("negatives not allowed - -32, -12, -8");
   });
 });
